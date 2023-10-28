85550fea17807f2900b098e834b502a307b09149
afonne-cid <delightinbusiness@gmail.com>
2023-10-28 05:30:14 +0100

    validate binary search tree solution

---
+++ python/00098-validate-binary-search-tree.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isValidBST(root: TreeNode) -> bool:
+    def valid(node, left, right):
+        if not node:
+            return True
+        if not (left < node.val < right):
+            return False
+        return valid(node.left, left, node.val) and valid(node.right, node.val, right)
+
+    return valid(root, float("-inf"), float("inf"))
