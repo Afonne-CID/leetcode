4c305495744bee2ce73f3d57bd90f774ee37a5d5
afonne-cid <delightinbusiness@gmail.com>
2023-10-28 05:38:20 +0100

    valid perfect square solution

---
+++ python/00367-valid-perfect-square.py
@@ -0,0 +1,13 @@
+def isPerfectSquare(num: int) -> bool:
+    left, right = 1, num
+
+    while left <= right:
+        mid = (left + right) // 2
+        if mid * mid > num:
+            right = mid - 1
+        elif mid * mid < num:
+            left = mid + 1
+        else:
+            return True
+
+    return False
