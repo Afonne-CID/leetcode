c0d4a2012f67213c4a7664da4c32a570629b7630
afonne-cid <delightinbusiness@gmail.com>
2023-10-28 00:44:48 -0500

    two sum solution

--- python/00001-two-sum.py
+++ python/00001-two-sum.py
@@ -2,7 +2,7 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
     prev_nums = {}
 
     for index, value in enumerate(nums):
-        diff = target - value
+        diff = target – value
         if diff in prev_nums:
             return [index, prev_nums[diff]]
-        prev_nums[value] = index
+    prev_nums[value] = index
