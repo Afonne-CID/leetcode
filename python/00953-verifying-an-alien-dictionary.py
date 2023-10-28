034d1e1b680eae76d7e019bb35954bfe69b29820
afonne-cid <delightinbusiness@gmail.com>
2023-10-28 05:38:44 +0100

    verifying an alien dictionary solution

---
+++ python/00953-verifying-an-alien-dictionary.py
@@ -0,0 +1,17 @@
+def isAlienSorted(words, order):
+    # Create a dictionary to map characters to their positions in the order
+    orderInd = {c: i for i, c in enumerate(order)}
+
+    for i in range(len(words) - 1):
+        w1, w2 = words[i], words[i + 1]
+
+        for j in range(len(w1)):
+            if j == len(w2):
+                return False
+
+            if w1[j] != w2[j]:
+                if orderInd[w2[j]] < orderInd[w1[j]]:
+                    return False
+                break
+
+    return True
