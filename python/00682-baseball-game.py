68b3a575d33242199f2a8b66f71a83c6a1906b9b
afonne-cid <delightinbusiness@gmail.com>
2023-10-28 05:38:33 +0100

    baseball game solution

---
+++ python/00682-baseball-game.py
@@ -0,0 +1,16 @@
+def calPoints(self, operations):
+    score_stack = []
+
+    for o in operations:
+        if o == '+' and len(score_stack) >= 2:
+            summed = score_stack[-2] + score_stack[-1]
+            score_stack.append(summed)
+        elif o == 'D' and len(score_stack) >= 1:
+            doubled = score_stack[-1] * 2
+            score_stack.append(doubled)
+        elif o == 'C' and len(score_stack) >= 1:
+            score_stack.pop()
+        else:
+            score_stack.append(int(o))
+
+    return sum(score_stack)
