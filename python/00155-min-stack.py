76bdc4a2eb58e7a59d45c677ed6bd3b719ecc73f
afonne-cid <delightinbusiness@gmail.com>
2023-10-28 00:45:05 -0500

    min stack solution

---
+++ python/00155-min-stack.py
@@ -0,0 +1,23 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []   # Stack to store elements
+        self.minStack = []  # Stack to store minimum values
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            if self.stack[-1] == self.minStack[-1]:
+                self.minStack.pop()
+            self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.minStack:
+            return self.minStack[-1]
