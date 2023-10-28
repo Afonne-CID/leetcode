0a6cfa518732e11388d06d831f3824e27f2539b3
afonne-cid <delightinbusiness@gmail.com>
2023-10-28 05:30:15 +0100

    maximum depth of binary tree solution

---
+++ python/00104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        q = deque()
+        q.append(root)
+        level = 0
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+
+        return level
