578f0da3344e1f26b7bb8d084536d56b6f784135
afonne-cid <delightinbusiness@gmail.com>
2023-10-28 05:38:22 +0100

    decode string solution

---
+++ python/00394-decode-string.py
@@ -0,0 +1,19 @@
+def decodeString(s: str) -> str:
+    stack = []
+
+    for char in s:
+        if char != "]":
+            stack.append(char)
+        else:
+            sub_str = ""
+            while stack[-1] != "[":
+                sub_str = stack.pop() + sub_str
+            stack.pop()
+
+            multiplier = ""
+            while stack and stack[-1].isdigit():
+                multiplier = stack.pop() + multiplier
+
+            stack.append(int(multiplier) * sub_str)
+
+    return "".join(stack)
