fa9195a4b38ac158b9aa750dd75919955f6fc371
afonne-cid <delightinbusiness@gmail.com>
2023-10-30 00:20:00 +0100

    find the kth largest integer in the array code fix

--- python/01985-find-the-kth-largest-integer-in-the-array.py
+++ python/01985-find-the-kth-largest-integer-in-the-array.py
@@ -1,7 +1,7 @@
-def kthLargestNumber(self, nums: List[str], k: int) -> str:
-    maxHeap = [-int(n) for n in nums]
-    heapq.heapify(maxHeap)
-    while k > 1:
-        heapq.heappop(maxHeap)
-        k -= 1
-    return str(-maxHeap[0])
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        maxHeap = [-int(n) for n in nums]
+        heapq.heapify(maxHeap)
+        while k > 1:
+            heapq.heappop(maxHeap)
+            k -= 1
+        return -maxHeap[0]
